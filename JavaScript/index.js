4c393076de3767cd77c4e165bc7faff2919ce094
Paula Moheb <poulamoheb12@gmail.com>
2024-03-12 03:51:06 +0200

    Adding getcomputerchoice function

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,7 +1,5 @@
 const choices=["Rock","Paper","Scissor"];
 function getComputerChoice(){
     let computerChoice =Math.floor(Math.random()*3);
-    console.log(choices[computerChoice]);
     return choices[computerChoice];
 }
-getComputerChoice();
